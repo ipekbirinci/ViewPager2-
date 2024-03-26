ddd2d4083142af008662fdcd1aa4f2dbe792769b
Ipek Birinci | Trader Technology <ipek.birinci@ttech.local>
2024-03-26 14:04:02 +0300

    final project

---
+++ app/src/main/java/com/example/viewpager2/ViewPagerAdapter.kt
@@ -0,0 +1,27 @@
+package com.example.viewpager2
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.viewpager2.daisy.DaisyFragment
+import com.example.viewpager2.rose.RoseFragment
+import com.example.viewpager2.violet.VioletFragment
+
+private const val NUM_TABS = 3
+
+public class ViewPagerAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
+    FragmentStateAdapter(fragmentManager, lifecycle) {
+
+    override fun getItemCount(): Int {
+        return NUM_TABS
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        when (position) {
+            0 -> return RoseFragment()
+            1 -> return VioletFragment()
+        }
+        return DaisyFragment()
+    }
+}
---
+++ app/src/main/java/com/example/viewpager2/daisy/DaisyFragment.kt
@@ -0,0 +1,30 @@
+package com.example.viewpager2.daisy
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.viewpager2.R
+
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+class DaisyFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_daisy, container, false)
+    }
+
+
+}
